6965c1f6b45d30d7b3ab29325e83954f01032984
TienBV <buivutien1508@gmail.com>
2025-12-09 17:41:19 +0700

    Update AdminViewTimeWorkingCenter.jsx

--- src/components/Admin/AdminManageRoom/AdminViewTimeWorkingCenter.jsx
+++ src/components/Admin/AdminManageRoom/AdminViewTimeWorkingCenter.jsx
@@ -525,7 +525,6 @@ const AdminViewTimeWorkingCenter = () => {
               onClick={() => setIsEditing(true)}
               className="inline-flex items-center px-6 py-2.5 bg-purple-600 text-white font-semibold rounded-lg hover:bg-purple-700 transition shadow-md hover:shadow-lg"
             >
-              <Pencil className="w-5 h-5 mr-2" />
               Cập nhật cấu hình
             </button>
           )}
